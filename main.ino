91c07b2a6c877ebac7078670799197cc508c25ab
Julian-Almario <julian-andres-almario@hotmail.com>
2024-08-25 13:29:35 -0500

    sensor humedad

--- main.ino
+++ main.ino
@@ -1,7 +1,21 @@
-void setup(){
-
+int SensorPin = A0;
+void setup()
+{
+    pinMode(7, OUTPUT);
+    Serial.begin(9600);
 }
 
-void loop(){
-
+void loop()
+{
+    int humedad = analogRead(SensorPin);
+    Serial.println(humedad);
+    if (humedad >= 460)
+    {
+        digitalWrite(7, LOW);
+    }
+    else
+    {
+        digitalWrite(7, HIGH);
+    }
+    delay(1000);
 }
